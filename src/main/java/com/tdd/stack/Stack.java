6861f28354e3c5d729ec82cd4c26d1e7390b276b
Rajesh Narayanaswomie <rajesh.narayanaswomi@wolterskluwer.com>
2017-09-28 15:36:31 -0400

    initial pass for integer stack

--- src/main/java/com/tdd/stack/Stack.java
+++ src/main/java/com/tdd/stack/Stack.java
@@ -1,5 +1,33 @@
 package com.tdd.stack;
 
-public class Stack {
+import java.util.ArrayList;
+import java.util.List;
 
+class Stack {
+    private int head = 0;
+    private List<Integer> itemList = new ArrayList<>();
+
+    int size() {
+        return head;
+    }
+
+    void push(final Integer item) {
+        itemList.add(item);
+        head++;
+    }
+
+    int pop() {
+        Integer removedItem = itemList.get(head - 1);
+        itemList.remove(head - 1);
+        head--;
+        return removedItem;
+    }
+
+    int peek() {
+        return itemList.get(head - 1);
+    }
+
+    boolean isEmpty() {
+        return itemList.size() == 0;
+    }
 }
--- src/test/java/com/tdd/stack/StackTest.java
+++ src/test/java/com/tdd/stack/StackTest.java
@@ -1,5 +1,73 @@
 package com.tdd.stack;
 
-public class StackTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class StackTest {
+    private Stack myStack;
+
+    @Before
+    public void setUp() throws Exception {
+        myStack = new Stack();
+    }
+
+    @Test
+    public void shouldReturnSizeAsZeroOnEmptyStack() throws Exception {
+        assertThat(myStack.size(), is(0));
+    }
+
+    @Test
+    public void shouldPushOneItemAndCheckSize() throws Exception {
+        myStack.push(20);
+        assertThat(myStack.size(), is(1));
+    }
+
+    @Test
+    public void shouldPopTheLastItemAndCheckSize() throws Exception {
+        myStack.push(10);
+        assertThat(myStack.pop(), is(10));
+        assertThat(myStack.size(), is(0));
+    }
+
+    @Test
+    public void shouldReturnLastItemAndRetain() throws Exception {
+        myStack.push(10);
+        assertThat(myStack.peek(), is(10));
+        assertThat(myStack.size(), is(1));
+    }
+
+    @Test
+    public void shouldReturnTrueOnEmptyStack() throws Exception {
+        assertTrue(myStack.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnFalseOnNonEmptyStack() throws Exception {
+        myStack.push(30);
+        assertFalse(myStack.isEmpty());
+    }
+
+    @Test
+    public void shouldWorkForMultipleValues() throws Exception {
+        assertTrue(myStack.isEmpty());
+
+        myStack.push(10);
+        myStack.push(20);
+        myStack.push(30);
+
+        assertThat(myStack.size(), is(3));
+        assertThat(myStack.peek(), is(30));
+        assertFalse(myStack.isEmpty());
+
+        myStack.pop();
+
+        assertThat(myStack.size(), is(2));
+        assertThat(myStack.peek(), is(20));
+        assertFalse(myStack.isEmpty());
+    }
 }
